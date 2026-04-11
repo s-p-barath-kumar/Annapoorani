f4fa941d0bdea4e53789878663e07b619a25418d
S P Barath Kumar <barathkumar.s.p21g333@gmail.com>
2026-04-11 19:43:14 +0530

    Sending email for registered student

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,8 +2,8 @@ import Student from "../models/Student.js";
 import Order from "../models/Order.js";
 import bcrypt from "bcryptjs";
 import StudentRequest from "../models/StudentRequest.js";
-// import { sendStudentCredentials } from "../services/emailService.js";
-import { sendStudentCredentialsSms } from "../services/smsService.js";
+import { sendStudentCredentials } from "../services/emailService.js";
+// import { sendStudentCredentialsSms } from "../services/smsService.js";
 
 export const getStudents = async (req, res) => {
 
@@ -67,9 +67,9 @@ export const registerStudent = async (req, res) => {
       ...data,
       password: hashedPassword
     });
-    // await sendStudentCredentials(req.body.email, req.body.regNo, password);
     console.log("Generated password for student:", password, "Sending SMS to:", req.body.phone);
-    const smsResponse = await sendStudentCredentialsSms(req.body.phone, req.body.regNo, password);
+    await sendStudentCredentials(req.body.email, req.body.regNo, password);
+    // const smsResponse = await sendStudentCredentialsSms(req.body.phone, req.body.regNo, password);
 
     console.log("SMS response:", smsResponse);
 
