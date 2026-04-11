8ca6262119e850b7a8efaaf32f771a72c2402728
S P Barath Kumar <barathkumar.s.p21g333@gmail.com>
2026-04-11 19:09:29 +0530

    ENV changes

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import path from "path";
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "production";
 
 const envPath = path.resolve(
   process.cwd(),
